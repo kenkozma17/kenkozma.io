ee934daba4e2bd6f3b33460e5556e4a34ce25648
Ken Kozma <kkozma@liquidfish.com>
2019-12-22 18:09:47 -0600

    Git Init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ken Kozma | Web Developer</title>
+
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+          <h1>Hello</h1>
+        </div>
+    </body>
+</html>
