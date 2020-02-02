8429c50603489c8243e557918a5178afb61c566b
Ken Kozma <kkozma@liquidfish.com>
2020-02-01 18:27:12 -0600

    Changed title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Ken Kozma | Web Developer</title>
+        <title>Ken Kozma | Web Artist</title>
 
         <? # CSS Bundle ?>
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
